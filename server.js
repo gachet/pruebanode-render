6fbbbcb89bf62afff25de57cce8f9b2c8d07d976
gachet <diegogachet@hotmail.com>
2024-08-06 21:45:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 require('dotenv').config();
-const bcrypt = require('bcryptjs');
 const http = require('http');
 const express = require('express');
 const RED = require('node-red');
 const app = express();
 const server = http.createServer(app);
-const users = [
-  { username: "admin", password: bcrypt.hashSync(process.env.ADMIN_PASSWORD || 'admin', 8), 
-   permissions: "*" }
-  ];
 const settings = {
   httpAdminRoot: "/",
   httpNodeRoot: "/api",
   userDir: "./",
   flowFile: 'flows.json',
-  credentialSecret: process.env.CREDENTIAL_SECRET || 'diego2000',
-   adminAuth: {
-    type: "credentials"
-  },
+  credentialSecret: process.env.CREDENTIAL_SECRET || ''your-local-secret-key',
   functionGlobalContext: {}
 };
 
